4df1662f86ba2c5d4f2b2706ae46fa57786200e5
therealsylaucoin <sylaucoin@gmail.com>
2020-12-06 13:02:19 -0500

    initially structure laid out

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+function Footer () {
+    return(
+        <footer>
+            <p>Made by the G6 Crew @ <a href="https://junocollege.com/">Juno College</a></p>
+            <p>Christina Yu, Dave Ratcliffe, Ermek Barmashev, Syl Aucoin</p>
+        </footer>
+    )
+}
+
+export default Footer;
